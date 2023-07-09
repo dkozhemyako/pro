589f1d0cebe08c958e6cbd80424a1cd200b87a5b
dkozhemyako <d.kozhemyako@icloud.com>
2023-07-09 15:15:03 +0300

    Show problem

--- app/Http/Requests/Book/BookShowRequest.php
+++ app/Http/Requests/Book/BookShowRequest.php
@@ -11,7 +11,12 @@ class BookShowRequest extends FormRequest
      *
      * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
      */
-
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'id' => $this->route('id'),
+        ]);
+    }
 
         public function rules(): array
     {
@@ -21,11 +26,5 @@ class BookShowRequest extends FormRequest
         ];
     }
 
-    protected function prepareForValidation(): void
-    {
-        $this->merge([
-            'id' => $this->route('id'),
-        ]);
-    }
 
 }
